26fa03d3034da9a8caf68dd7ceb6912c4607403b
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-06-07 14:16:30 -0300

    Create home page

--- website/__init__.py
+++ website/__init__.py
@@ -3,4 +3,11 @@ from flask import Flask
 def create_app():
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = 'secret key for our app not share on production'
+
+	from .views import views
+	from .auth import auth
+
+	app.register_blueprint(views, url_prefix='/')
+	app.register_blueprint(auth, url_prefix='/')
+
 	return app
