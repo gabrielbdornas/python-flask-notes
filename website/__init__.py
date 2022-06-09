a496052141818728834123dcdee1901c9df91dd4
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-06-09 14:31:00 -0300

    Create db setup and models

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlquemy
+
+db = SQLAlquemy()
+DB_NAME = "database.db"
 
 def create_app():
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = 'secret key for our app not share on production'
+	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+	db.init_app(app)
 
 	from .views import views
 	from .auth import auth
