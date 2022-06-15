4f07de030bc87687205a13f8422c4a2ea20fd56d
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-06-15 09:05:03 -0300

    Create db and fix typos

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlquemy
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-db = SQLAlquemy()
+db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
@@ -16,4 +17,13 @@ def create_app():
 	app.register_blueprint(views, url_prefix='/')
 	app.register_blueprint(auth, url_prefix='/')
 
+	from .models import User, Note
+
+	create_database(app)
+
 	return app
+
+def create_database(app):
+	if not path.exists('website/' + DB_NAME):
+		db.create_all(app=app)
+		print('Created Database!')
--- website/models.py
+++ website/models.py
@@ -14,5 +14,5 @@ class Note(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	data = db.Column(db.String(10000))
 	date = db.Column(db.DateTime(timezone=True), default=func.now())
-	user_id = db.Column(db.Integer, db.ForeingKey('user.id'))
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	
