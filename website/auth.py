7396eeb7041b96cb28dd40b3642c4c17f97a45dd
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-06-20 07:47:51 -0300

    Add logged in user functionalities

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -14,6 +15,7 @@ def login():
 		if user:
 			if check_password_hash(user.password, password):
 				flash('Logged in successfully!', category='success')
+				login_user(user, remember=True)
 				return redirect(url_for('views.home'))
 			else:
 				flash('Incorrect password, try again.', category='error')
@@ -24,8 +26,10 @@ def login():
 	return render_template('login.html')
 
 @auth.route('/logout')
+@login_required
 def logout():
-	return "<h1>Logout</h1>"	
+	logout_user()
+	return redirect(url_for('auth.login'))	
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -53,6 +57,7 @@ def sign_up():
 							)
 			db.session.add(new_user)
 			db.session.commit()
+			login_user(user, remember=True)
 			flash('Account created!', category='success')
 			return redirect(url_for('views.home'))			
 
